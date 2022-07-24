eafa9099c51e103685ab0032588c8ab6e8c07c28
zhangzhengyang <1074385735@qq.com>
2022-07-24 21:56:04 +0800

    搭建eureka server 高可用集群、通过eurake调用服务

--- lagou-parent/lagou-service-page/src/main/java/com/lagou/page/controller/PageController.java
+++ lagou-parent/lagou-service-page/src/main/java/com/lagou/page/controller/PageController.java
@@ -2,6 +2,8 @@ package com.lagou.page.controller;
 
 import com.lagou.common.pojo.Products;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,15 +11,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/page")
 public class PageController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/getData/{id}")
     public Products findDataById(@PathVariable Integer id) {
-        Products products = restTemplate.getForObject("http://localhost:9000/product/query/" + id, Products.class);
+        // Products products = restTemplate.getForObject("http://localhost:9000/product/query/" + id, Products.class);
+        // System.out.println("从lagou-service-product获得product对象:" + products);
+        // return products;
+
+
+        //1.获得Eureka中注册的lagou-service-product实例集合
+        List<ServiceInstance> instances = discoveryClient.getInstances("lagou-service-product");
+        // 2.获得实例集合中的第一个
+        ServiceInstance instance = instances.get(0);
+
+        //3.根据实例信息拼接IP地址
+        String host = instance.getHost();
+        int port = instance.getPort();
+        String url = "http://" + host + ":" + port + "/product/query/" + id;
+
+        //4.调用
+        Products products = restTemplate.getForObject(url, Products.class);
         System.out.println("从lagou-service-product获得product对象:" + products);
         return products;
     }
