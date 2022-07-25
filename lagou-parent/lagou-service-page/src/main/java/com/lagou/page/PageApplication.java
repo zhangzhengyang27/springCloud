9a2240d6b7d0e2953d3f7c2a4512d7b685843392
zhangzhengyang <1074385735@qq.com>
2022-07-25 20:10:54 +0800

    改为nacos项目并注册成功

--- lagou-parent/lagou-service-page/src/main/java/com/lagou/page/PageApplication.java
+++ lagou-parent/lagou-service-page/src/main/java/com/lagou/page/PageApplication.java
@@ -2,16 +2,12 @@ package com.lagou.page;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient //@EnableEurekaClient
-// @EnableCircuitBreaker // 开启熔断,只需要配置这个
 @EnableFeignClients // 开启Feign
 public class PageApplication {
 
--- lagou-parent/lagou-service-product/src/main/java/com/lagou/product/ProductApplication.java
+++ lagou-parent/lagou-service-product/src/main/java/com/lagou/product/ProductApplication.java
@@ -3,7 +3,7 @@ package com.lagou.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
